c5b69e466fc92cb15dc3ec663e02977592f4c0b4
shanmathiravikumar <139548343+shanmathiravikumar@users.noreply.github.com>
2023-07-17 16:50:04 +0530

    Add files via upload

---
+++ Oddeven.java
@@ -0,0 +1,24 @@
+package com.shan.numbers;
+import java.util.Scanner;
+
+
+
+public class Oddeven {
+	public static void main(String args[]) {
+		int num;
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter number to check if it is odd or even:");
+		num =scanner.nextInt();
+		
+		if(num%2==0) {
+			System.out.println(num +"is even");
+		}
+		else {
+			System.out.println(num + "is odd");
+		}
+
+	}
+	
+}
+
+//check whether the given number is odd or even
